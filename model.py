d1efaca7a5a5b37356cf8aff84abc34267d773ed
Yahu Gao <gao_yahu@163.com>
2020-03-25 19:54:21 -0400

    Test model
    Signed-off-by: Yahu Gao <gao_yahu@163.com>

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import numpy
 import os
 import torch
 from flyai.model.base import Base
+from flyai.dataset import Dataset
+from net import Net
 
 from path import MODEL_PATH
 
@@ -14,6 +16,7 @@ TORCH_MODEL_NAME = "model.pkl"
 class Model(Base):
     def __init__(self, data):
         self.data = data
+        self.net = None
         self.net_path = os.path.join(MODEL_PATH, TORCH_MODEL_NAME)
         if os.path.exists(self.net_path):
             self.net = torch.load(self.net_path)
@@ -21,6 +24,7 @@ class Model(Base):
     def predict(self, **data):
         if self.net is None:
             self.net = torch.load(self.net_path)
+
         x_data = self.data.predict_data(**data)
         x_data = torch.from_numpy(x_data)
         outputs = self.net(x_data)
@@ -31,14 +35,13 @@ class Model(Base):
     def predict_all(self, datas):
         if self.net is None:
             self.net = torch.load(self.net_path)
+        datas = torch.Tensor(datas)
+        outputs = self.net(datas)
+        predictions = outputs.data.numpy()
         labels = []
-        for data in datas:
-            x_data = self.data.predict_data(**data)
-            x_data = torch.from_numpy(x_data)
-            outputs = self.net(x_data)
-            prediction = outputs.data.numpy()
-            prediction = self.data.to_categorys(prediction)
-            labels.append(prediction)
+        for prediction in predictions:
+            label = self.data.to_categorys(prediction)
+            labels.append(label)
         return labels
 
     def batch_iter(self, x, y, batch_size=128):
@@ -58,3 +61,11 @@ class Model(Base):
     def save_model(self, network, path, name=TORCH_MODEL_NAME, overwrite=False):
         super().save_model(network, path, name, overwrite)
         torch.save(network, os.path.join(path, name))
+
+
+if __name__ == '__main__':
+    dataset = Dataset(10, 32)
+    model = Model(dataset)
+    x_vals, y_vals = dataset.next_validation_batch()
+    labels = model.predict_all(x_vals)
+    print(labels)
